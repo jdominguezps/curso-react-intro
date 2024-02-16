9725ae816f93e4d166a726ffebfabbf260a670f8
Juan Carlos Domínguez <jdominguez@priceshoes.com>
2024-02-15 20:57:24 -0600

    Maquetando formularios en React

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -3,18 +3,23 @@ import './TodoForm.css';
 
 function TodoForm(){
     return(
-        <form>
+        <form onSubmit={(event) => {
+            event.preventDefault();            
+        }}>
             <label>Agrega un nuevo TODO</label>
             <textarea
+                className="NewTodo"
                 placeholder="Inserta aqui..."
             />
 
             <div className="TodoForm-buttonContainer">
                 <button
+                    type=""
                     className="TodoForm-button
                     TodoForm-button--cancel"
                 >Cancelar</button>
                 <button
+                    type="submit"
                     className="TodoForm-button
                     TodoForm-button--add"
                 >Agregar</button>
